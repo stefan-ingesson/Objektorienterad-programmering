f89a8aa01b5689fc8f25bc9501f32295977c2709
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-01-14 15:00:38 +0100

    Laddat upp övningen Produkten av heltal

---
+++ Testhörna/Övningar/Produkt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace produkt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // deklarera variabler
+            long produkt = 1;
+
+            //uträkning
+            for (int i = 1; i < 21; i+=1)
+            {
+                produkt *= i;
+
+                
+            }
+
+            // presentation av resultat
+            Console.WriteLine("Produkten av alla heltal mellan 1-20 är {0}", produkt);
+        }
+    }
+}
