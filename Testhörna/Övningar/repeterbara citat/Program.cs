d9b20d9ac0fb0421b253cc5aa130dcfb470a1a83
Stefan Ingesson <stefan.ingesson@gmail.com>
2015-01-25 14:05:21 +0100

    Test för metoder

---
+++ Testhörna/Övningar/repeterbara citat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repeterbara_citat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuotationViewer qw = new QuotationViewer();
+            qw.Quotation = "\"I have a dream\"";
+            qw.Count = 7;
+            qw.View();
+
+            QuotationViewer qwNew = new QuotationViewer();
+            qwNew.Quotation = "\"Make love, not war\"";
+            qwNew.Count = 3;
+            qwNew.View();
+
+            QuotationViewer qwNewQuote = new QuotationViewer();
+            qwNewQuote.Quotation = "\"Et tu, Brute\"";
+            qwNewQuote.Count = 3;
+            qwNewQuote.View();
+        }
+    }
+}
---
+++ Testhörna/Övningar/repeterbara citat/QuotationViewer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repeterbara_citat
+{
+   public class QuotationViewer
+    {
+        private int _count;
+
+        public int Count
+        {
+            get { return _count; }
+            set { _count = value; }
+        }
+
+        private string _quotation;
+
+        public string Quotation
+        {
+            get { return _quotation; }
+            set { _quotation = value; }
+        }
+
+        public QuotationViewer()
+        {
+            _quotation = null;
+            _count = 0;
+        }
+
+        public QuotationViewer(string quotation, int count)
+        {
+            _quotation = quotation;
+            _count = count;
+        }
+
+        public void View()
+        {
+            Console.WriteLine("------------");
+            for (int i = 0; i < _count; i++)
+            {
+                Console.WriteLine(_quotation);
+            }
+            Console.WriteLine();
+        }
+   }
+}
